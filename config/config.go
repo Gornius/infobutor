31b5b01d99705ce47b87a4b10a329b55562ec9be
gornius <ninertominer@gmail.com>
2024-04-23 23:01:16 +0200

    Refactor of config files

--- config/config.go
+++ config/config.go
@@ -17,9 +17,8 @@ type Config struct {
 }
 
 type ConfigParser interface {
-	FromFile(manager *manager.Manager, file []byte) (*Config, error)
-	FromMap(senderManager *manager.Manager, configMap map[string]any) (*Config, error)
-	CreateDefault(path string) error
+	FromFile(manager *manager.Manager, file []byte) (map[string]any, error)
+	ToFile(path string, configMap map[string]any) error
 }
 
 func DefaultLocation() string {
@@ -36,7 +35,7 @@ func FromFile(manager *manager.Manager, path string) (*Config, error) {
 	}
 
 	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
-		if err := parser.CreateDefault(path); err != nil {
+		if err := parser.ToFile(path, GetDefaultConfig()); err != nil {
 			return nil, err
 		}
 	} else if err != nil {
@@ -48,5 +47,64 @@ func FromFile(manager *manager.Manager, path string) (*Config, error) {
 		return nil, err
 	}
 
-	return parser.FromFile(manager, file)
+	configMap, err := parser.FromFile(manager, file)
+	if err != nil {
+		return nil, err
+	}
+
+	return FromMap(manager, configMap)
+}
+
+func FromMap(senderManager *manager.Manager, configMap map[string]any) (*Config, error) {
+	senders := map[string]sender.Sender{}
+	if configMap["channels"] != nil {
+		sendersConfigMap := configMap["senders"].(map[string]any)
+		for senderIdentifier, senderConfig := range sendersConfigMap {
+			newSender, err := senderManager.SenderFromConfig(senderConfig.(map[string]any))
+			if err != nil {
+				return nil, err
+			}
+			senders[senderIdentifier] = newSender
+		}
+	}
+
+	channels := map[string]*channel.Channel{}
+	if configMap["channels"] != nil {
+		channelConfigMap := configMap["channels"].(map[string]any)
+		for channelId, channelConfig := range channelConfigMap {
+			channelSenders := []sender.Sender{}
+			for _, senderId := range channelConfig.(map[string]any)["senders"].([]any) {
+				channelSenders = append(channelSenders, senders[senderId.(string)])
+			}
+			channels[channelId] = &channel.Channel{
+				Token:   channelConfig.(map[string]any)["token"].(string),
+				Senders: channelSenders,
+			}
+		}
+	}
+
+	config := new(Config)
+
+	config.Senders = senders
+	config.Channels = channels
+
+	return config, nil
+}
+
+func GetDefaultConfig() map[string]any {
+	return map[string]any{
+		"senders": map[string]any{
+			"my_telegram": map[string]any{
+				"type":      "telegram",
+				"api_token": "YOUR_API_TOKEN",
+				"chat_id":   "-123456789",
+			},
+		},
+		"channels": map[string]any{
+			"default": map[string]any{
+				"token":   "2018s1m0eme0i1we21ps21",
+				"senders": []string{"my_telegram"},
+			},
+		},
+	}
 }
--- config/yaml.go
+++ config/yaml.go
@@ -4,8 +4,6 @@ import (
 	_ "embed"
 	"os"
 
-	"github.com/gornius/infobutor/channel"
-	"github.com/gornius/infobutor/sender"
 	"github.com/gornius/infobutor/sender/manager"
 	"gopkg.in/yaml.v3"
 )
@@ -13,61 +11,26 @@ import (
 type YamlParser struct {
 }
 
-func (yp *YamlParser) FromFile(senderManager *manager.Manager, file []byte) (*Config, error) {
+func (yp *YamlParser) FromFile(senderManager *manager.Manager, file []byte) (map[string]any, error) {
 	configMap := map[string]any{}
 
 	if err := yaml.Unmarshal(file, configMap); err != nil {
 		return nil, err
 	}
-	return yp.FromMap(senderManager, configMap)
+	return configMap, nil
 }
 
-// TODO: move all of this to config - no reason to make this yaml specific
-func (yp *YamlParser) FromMap(senderManager *manager.Manager, configMap map[string]any) (*Config, error) {
-	senders := map[string]sender.Sender{}
-	if configMap["channels"] != nil {
-		sendersConfigMap := configMap["senders"].(map[string]any)
-		for senderIdentifier, senderConfig := range sendersConfigMap {
-			newSender, err := senderManager.SenderFromConfig(senderConfig.(map[string]any))
-			if err != nil {
-				return nil, err
-			}
-			senders[senderIdentifier] = newSender
-		}
-	}
-
-	channels := map[string]*channel.Channel{}
-	if configMap["channels"] != nil {
-		channelConfigMap := configMap["channels"].(map[string]any)
-		for channelId, channelConfig := range channelConfigMap {
-			channelSenders := []sender.Sender{}
-			for _, senderId := range channelConfig.(map[string]any)["senders"].([]any) {
-				channelSenders = append(channelSenders, senders[senderId.(string)])
-			}
-			channels[channelId] = &channel.Channel{
-				Token:   channelConfig.(map[string]any)["token"].(string),
-				Senders: channelSenders,
-			}
-		}
-	}
-
-	config := new(Config)
-
-	config.Senders = senders
-	config.Channels = channels
-
-	return config, nil
-
-}
-
-//go:embed default_configs/conf.yml
-var defaultYaml []byte
-
-func (yp *YamlParser) CreateDefault(path string) error {
+func (yp *YamlParser) ToFile(path string, configMap map[string]any) error {
 	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0664)
 	if err != nil {
 		return err
 	}
-	file.Write(defaultYaml)
+
+	encoder := yaml.NewEncoder(file)
+	encoder.SetIndent(2)
+	if err := encoder.Encode(configMap); err != nil {
+		return err
+	}
+
 	return nil
 }
