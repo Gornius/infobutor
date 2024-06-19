72a0098b6c9e429d0b7fce66cd1619c3128cb88b
gornius <ninertominer@gmail.com>
2024-06-20 01:03:49 +0200

    Move command to cobra framework

--- cmd/infobutor/main.go
+++ cmd/infobutor/main.go
@@ -1,14 +1,80 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"runtime"
+
 	"github.com/gornius/infobutor"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	app, err := infobutor.NewDefaultApp()
+	execute()
+}
+
+func execute() {
+	rootCmd := &cobra.Command{
+		Use:   "infobutor",
+		Short: "Infobutor is a program that handles distributing messages to user-defined sources",
+		Run: func(cmd *cobra.Command, args []string) {
+			app, err := infobutor.NewDefaultApp()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			app.Router.Logger.Fatal(app.Router.Start(":" + cmd.Flag("port").Value.String()))
+		},
+	}
+	rootCmd.PersistentFlags().StringP("port", "p", "3000", "Port you want to run infobutor at")
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "reload",
+		Short: "Reloads configuration",
+		Run: func(cmd *cobra.Command, args []string) {
+			port := cmd.Flag("port").Value.String()
+			err := sendReloadRequest(port)
+			if err != nil {
+				fmt.Println(err)
+			}
+		},
+	})
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func sendReloadRequest(port string) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			msg := r.(*runtime.TypeAssertionError).Error()
+			err = errors.New("Got bad response from server: " + msg)
+		}
+	}()
+
+	data := map[string]any{}
+	jsonData, err := json.Marshal(data)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
-	app.Router.Logger.Fatal(app.Router.Start(":3000"))
+	response, err := http.Post("http://127.0.0.1:"+port+"/reload-config", "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	var responseData map[string]any
+	json.NewDecoder(response.Body).Decode(&responseData)
+
+	if !responseData["configReloaded"].(bool) {
+		return errors.New("couldn't reload config")
+	}
+
+	return err
 }
